e0664fe03ebfe65f9791d15126141e57c0d1de3f
Rendenwald <mr.rendenwald@gmail.com>
2024-02-23 21:03:41 +0100

    Integrate cookieconsent

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,3 +2,10 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 /// <reference types="vite/client" />
+
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+// TODO: tidy cookie consent config
+
+export const COOKIECONSENTCONFIG = {};
